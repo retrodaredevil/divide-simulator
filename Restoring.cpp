117cf384aa143bc9f80d6d7c2f3ce61a0fe59285
Drew Missler <dcmbdc@mst.edu>
2022-11-15 18:21:53 -0600

    added code!

--- Restoring.cpp
+++ Restoring.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Restoring.h"
+
+Result restoringMethod(const TestElement & element)
+{
+  long EAQ = element.dividend;
+  long bitMask = (1<<(element.divisorLength+1)) - 1;
+  long B2sComp = (((~element.divisor + 1) & bitMask) << element.divisorLength);
+
+  for(int i = 0; i < element.divisorLength; i++)
+  {
+    EAQ <<= 1;
+    EAQ -= B2sComp;
+  }
+}
